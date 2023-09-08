17cce172bc8b7b3fbf067af7713948f273e2c3af
VinuGore26 <vvgore2675@gmail.com>
2023-09-08 12:57:00 +0530

    Adding some files with upadation of looping news components, pages.Added Previous & Next buttons to the page for navigation between the pages and for to read the more news.There is the logic of parsedData to collect news data in JSON and to further used in our project.

--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -8,11 +8,11 @@ export default class NewsItems extends Component {
       <>
         <div className="container my-3">
             <div className="card" style={{width: '18rem'}}>
-                <img src={imageUrl} className="card-img-top" alt=""/>
+                <img src={!imageUrl?"https://media.cnn.com/api/v1/images/stellar/prod/230906120920-03-donald-trump-082423.jpg?c=16x9&q=w_800,c_fill":imageUrl} className="card-img-top" alt=""/>
                 <div className="card-body">
                     <h5 className="card-title">{title}..</h5>
                     <p className="card-text">{description}..</p>
-                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
         </div>
